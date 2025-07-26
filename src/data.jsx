2d167e9208b1051a83b2f29336346f3c03e8b2c9
pujakul1942@gmail.com <pujakul1942@gmail.com>
2025-07-26 13:09:21 +0530

    Trip website

---
+++ src/data.jsx
@@ -0,0 +1,60 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info:"Agra is a historic city in Uttar Pradesh, India, famous for the Taj Mahal, a UNESCO World Heritage Site and one of the Seven Wonders of the World. It also boasts landmarks like the Agra Fort and Fatehpur Sikri. Once the capital of the Mughal Empire, Agra is rich in culture, known for its Mughlai cuisine and sweets like Petha. Located on the Yamuna River ",
+        image: "https://static.toiimg.com/photo/msid-31346158,width-96,height-65.cms ",
+        price: "55,234",
+    },
+    {
+        id: 2,
+        name: "Goa",
+        info:"Goa is a coastal state in western India, known for its beautiful beaches, vibrant nightlife, Portuguese-influenced architecture, and rich cultural heritage. Popular beaches include Baga, Anjuna, and Palolem. Goa’s highlights include historic churches like Basilica of Bom Jesus, spice plantations, and water sports. The state is famous for its lively festivals, seafood, and Goan cuisine. ",
+        image: " https://media.istockphoto.com/id/181691319/photo/goas-idyllic-beach.jpg?s=612x612&w=0&k=20&c=BcYsjYGJ-PPhx7VQoiENFAC200V4hMoRzRpx3c2Xd-s= ",
+        price: "55,234",
+    },
+    {
+        id: 3,
+        name: "Jaipur",
+        info:"Jaipur, the capital of Rajasthan, is known as the Pink City for its distinct pink-colored buildings. It’s famous for its royal palaces, forts, and vibrant culture. Key attractions include the Hawa Mahal, City Palace, Amber Fort, and Jantar Mantar (a UNESCO World Heritage Site). Jaipur is part of the Golden Triangle tourist circuit with Delhi and Agra. ",
+        image: "https://static2.tripoto.com/media/filter/tst/img/500534/TripDocument/1647064199_244444313_4467549076658082_8202287498074542143_n.jpeg",
+        price: "55,234",
+    },
+    {
+        id: 4,
+        name: "Leh-Ladakh",
+        info: "Leh-Ladakh, located in the Union Territory of Ladakh, is known for its stunning mountain landscapes, crystal-clear lakes, and ancient Buddhist monasteries. Key attractions include Pangong Lake, Nubra Valley, Magnetic Hill, Leh Palace, and Thiksey Monastery. It’s a paradise for adventure enthusiasts, offering trekking, biking, and river rafting. ",
+        image:"https://i.pinimg.com/550x/5b/e3/91/5be391258bb18d986c977fd45ae755dd.jpg",
+        price: "55,234",
+    },
+    {
+        id: 5,
+        name: "Darjeeling",
+        info:"Darjeeling, located in West Bengal, is a hill station known for its scenic tea gardens, stunning views of the Himalayas, and the  Kanchenjunga peak. Key attractions include the Darjeeling Himalayan Railway (Toy Train), Tiger Hill, Batasia Loop, and Peace Pagoda.",
+        image: "https://darjeelingyatra.com/uploads/qw-750-450.jpg ",
+        price: "55,234",
+    },
+    {
+        id: 6,
+        name: "Rishikesh",
+        info:"Rishikesh, located in Uttarakhand, is known as the Yoga Capital of the World and a major hub for spirituality, adventure sports, and nature. It’s famous for its temples, including Triveni Ghat, Neelkanth Mahadev Temple, and the Lakshman Jhula. Rishikesh is also a popular destination for rafting, trekking, and camping along the Ganges River.  ",
+        image: "https://ihplb.b-cdn.net/wp-content/uploads/2019/02/river-rafting-rishikesh.png ",
+        price: "55,234",
+    },
+    {
+        id: 7,
+        name: "Kolkata",
+        info:" Kolkata, the capital of West Bengal, is known as the City of Joy for its vibrant culture, colonial architecture, and literary heritage. Key attractions include the Victoria Memorial, Howrah Bridge, Dakshineswar Temple, and Indian Museum. It’s famous for Durga Puja, street food like puchka and rasgulla, and its rich art and literature scene.",
+        image: "https://media.istockphoto.com/id/1164386039/photo/howrah-bridge-on-river-ganges-at-kolkata-at-twilight-with-moody-sky.jpg?s=612x612&w=0&k=20&c=CHrNWdInFSDyERdvgd0f8935hZcBQU6lbYCE4LlXqUY=",
+        price: "55,234",
+    },
+    {
+        id: 8,
+        name: "Delhi",
+        info:"Delhi, the capital of India, is a vibrant city known for its rich history, diverse culture, and bustling markets. It’s home to iconic landmarks like the Red Fort, India Gate, Qutub Minar, Lotus Temple, and Humayun’s Tomb. The city blends ancient monuments with modern architecture and is famous for its street food, shopping hubs, and festivals. ",
+        image: "https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg ",
+        price: "55,234",
+    },
+]
+
+export default data;
